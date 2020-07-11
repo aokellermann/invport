0615526ea097e6a12ea989061c1e77cf3758dc15
Antony Kellermann <aokellermann@gmail.com>
2020-07-11 14:33:23 -0400

    Added setter/getter

--- invport/detail/cache.h
+++ invport/detail/cache.h
@@ -18,13 +18,11 @@ namespace inv
 {
 class Cache : private file::FileIoBase, private json::JsonBidirectionalSerializable
 {
-  using EndpointMap = std::unordered_map<iex::Endpoint::Type, iex::EndpointPtr<>>;
-  using SecurityIndex = iex::SymbolMap<EndpointMap>;
-
  public:
   using Endpoint = iex::Endpoint;
   using SymbolEndpoint = iex::SymbolEndpoint;
   using Symbol = iex::Symbol;
+  using EndpointType = iex::Endpoint::Type;
 
   template <typename E = Endpoint>
   using EndpointPtr = iex::EndpointPtr<E>;
@@ -35,10 +33,35 @@ class Cache : private file::FileIoBase, private json::JsonBidirectionalSerializa
 
   ErrorCode Flush() const;
 
+  template <EndpointType Type>
+  EndpointPtr<iex::EndpointTypename<Type>> Get(const Symbol& symbol) const
+  {
+    const auto stock_index_iter = stock_index_.find(symbol);
+    if (stock_index_iter != stock_index_.end())
+    {
+      const auto endpoint_map_iter = stock_index_iter->second.find(Type);
+      if (endpoint_map_iter != stock_index_iter->second.end())
+      {
+        return std::dynamic_pointer_cast<iex::EndpointTypename<Type>>(endpoint_map_iter->second);
+      }
+    }
+
+    return nullptr;
+  }
+
+  template <EndpointType Type>
+  void Set(EndpointPtr<> endpoint_ptr, const Symbol& symbol)
+  {
+    stock_index_[symbol][Type] = std::move(endpoint_ptr);
+  }
+
  private:
   ValueWithErrorCode<json::Json> Serialize() const final;
 
   ErrorCode Deserialize(const json::Json& input_json) final;
+
+  using EndpointMap = std::unordered_map<EndpointType, EndpointPtr<>>;
+  using SecurityIndex = iex::SymbolMap<EndpointMap>;
 
   SecurityIndex stock_index_;
 };
